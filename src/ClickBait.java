9a00839c1c31ba09070cb337dd5330fdad070baa
“Mitchell <mapr323@gmail.com>
2016-10-18 06:47:30 -0400

    Bot works with twitter! (Removed the secret key stuff). https://twitter.com/TheClickbaitBot

--- src/ClickBait.java
+++ src/ClickBait.java
@@ -11,19 +11,21 @@ public class ClickBait {
 		String action = null;
 		String bnews = null;
 		Random rand = new Random();
-
+		Calendar cal = Calendar.getInstance();
+		
 		// People
 		String per1 = "Obama";
 		String per2 = "Donald Trump";
 		String per3 = "Bernie Sanders";
-		String per4 = "Hillary Clinton";
+		String per4 = "Hillary Clintion";
 		String per5 = "Madonna";
 		String per6 = "Tim Cook";
 		String per7 = "Sergey Brin";
 		String per8 = "Larry Page";
 		String per9 = "Edward Snowden";
+		String per10 = "Ozzy Osbourne";
 
-		int pe1 = rand.nextInt(9);
+		int pe1 = rand.nextInt(10);
 		int pe2 = rand.nextInt(8);
 		switch (pe1) {
 		case 0:
@@ -53,11 +55,17 @@ public class ClickBait {
 			case 7:
 				p2 = per9;
 				break;
+			case 8:
+				p2 = per10;
+				break;
 			}
 			break;
 		case 1:
 			p1 = per2;
 			switch (pe2) {
+			case 8:
+				p2 = per10;
+				break;
 			case 0:
 				p2 = per1;
 				break;
@@ -87,6 +95,9 @@ public class ClickBait {
 		case 2:
 			p1 = per3;
 			switch (pe2) {
+			case 8:
+				p2 = per10;
+				break;
 			case 0:
 				p2 = per1;
 				break;
@@ -116,6 +127,9 @@ public class ClickBait {
 		case 3:
 			p1 = per4;
 			switch (pe2) {
+			case 8:
+				p2 = per10;
+				break;
 			case 0:
 				p2 = per1;
 				break;
@@ -145,6 +159,9 @@ public class ClickBait {
 		case 4:
 			p1 = per5;
 			switch (pe2) {
+			case 8:
+				p2 = per10;
+				break;
 			case 0:
 				p2 = per1;
 				break;
@@ -174,6 +191,9 @@ public class ClickBait {
 		case 5:
 			p1 = per6;
 			switch (pe2) {
+			case 8:
+				p2 = per10;
+				break;
 			case 0:
 				p2 = per1;
 				break;
@@ -205,6 +225,9 @@ public class ClickBait {
 			switch (pe2) {
 			case 0:
 				p2 = per1;
+				break;
+			case 8:
+				p2 = per10;
 				break;
 			case 1:
 				p2 = per2;
@@ -261,6 +284,9 @@ public class ClickBait {
 		case 8:
 			p1 = per9;
 			switch (pe2) {
+			case 8:
+				p2 = per10;
+				break;
 			case 0:
 				p2 = per1;
 				break;
@@ -287,6 +313,38 @@ public class ClickBait {
 				break;
 			}
 			break;
+		case 9:
+			p1 = per10;
+			switch (pe2) {
+			case 8:
+				p2 = per10;
+				break;
+			case 0:
+				p2 = per1;
+				break;
+			case 1:
+				p2 = per2;
+				break;
+			case 2:
+				p2 = per3;
+				break;
+			case 3:
+				p2 = per4;
+				break;
+			case 4:
+				p2 = per5;
+				break;
+			case 5:
+				p2 = per6;
+				break;
+			case 6:
+				p2 = per7;
+				break;
+			case 7:
+				p2 = per9;
+				break;
+			}
+			break;
 		}
 
 		System.out.println(p1);
@@ -300,9 +358,10 @@ public class ClickBait {
 		String act4 = " is angry at ";
 		String act5 = " wants to hire ";
 		String act6 = " wants to kill ";
+		String act7 = " wants to marry ";
 
 		// picks action
-		int act = rand.nextInt(6);
+		int act = rand.nextInt(7);
 		switch (act) {
 		case 0:
 			action = act1;
@@ -322,6 +381,9 @@ public class ClickBait {
 		case 5:
 			action = act6;
 			break;
+		case 6:
+			action = act7;
+			break;
 		}
 
 		System.out.println(action);
@@ -337,12 +399,37 @@ public class ClickBait {
 			break;
 		case 2:
 			bnews = "Urgent Bulletin: ";
+			break;
 		}
 
 		System.out.println(bnews);
 
 		// prints final clickbait title
 		System.out.println(bnews + p1 + action + p2 + "!");
+		
+		try (PrintWriter out = new PrintWriter("Clickbait " + cal.getTimeInMillis() + ".txt")) {
+
+			out.println("Enjoy your clickbait!");
+			out.println(bnews + p1 + action + p2 + "!");
+
+		}
+		try {
+			Twitter twitter = new TwitterFactory().getInstance();
+
+			twitter.setOAuthConsumer(consumerKeyStr, consumerSecretStr);
+			AccessToken accessToken = new AccessToken(accessTokenStr,
+					accessTokenSecretStr);
+
+			twitter.setOAuthAccessToken(accessToken);
+
+			//Tweet Text here!
+			twitter.updateStatus(bnews + p1 + action + p2 + "! #TheBaitBot");
+
+			System.out.println("Successfully updated the status in Twitter.");
+		} catch (TwitterException te) {
+			te.printStackTrace();
+		}
+		
 	}
 
 }
