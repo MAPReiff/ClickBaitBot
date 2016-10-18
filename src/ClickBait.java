e319ceaa50c3b64cf018209ca55d8f4f97d3d518
“Mitchell <mapr323@gmail.com>
2016-10-18 17:58:36 -0400

    Re did the picking mechanics. Added some people.

--- src/ClickBait.java
+++ src/ClickBait.java
@@ -1,435 +1,112 @@
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Random;
+import java.util.Scanner;
+
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.auth.AccessToken;
 
 public class ClickBait {
 
-	public static void main(String args[]) {
+	static String consumerKeyStr = "";
+	static String consumerSecretStr = "";
+	static String accessTokenStr = "";
+	static String accessTokenSecretStr = "";
 
-		// declaring main variables
-		String p1 = null;
-		String p2 = null;
-		String action = null;
-		String bnews = null;
+	public static void main(String args[]) throws FileNotFoundException {
+
+		// Declaring main variables
 		Random rand = new Random();
 		Calendar cal = Calendar.getInstance();
-		
-		// People
-		String per1 = "Obama";
-		String per2 = "Donald Trump";
-		String per3 = "Bernie Sanders";
-		String per4 = "Hillary Clintion";
-		String per5 = "Madonna";
-		String per6 = "Tim Cook";
-		String per7 = "Sergey Brin";
-		String per8 = "Larry Page";
-		String per9 = "Edward Snowden";
-		String per10 = "Ozzy Osbourne";
 
-		int pe1 = rand.nextInt(10);
-		int pe2 = rand.nextInt(8);
-		switch (pe1) {
-		case 0:
-			p1 = per1;
-			switch (pe2) {
-			case 0:
-				p2 = per2;
-				break;
-			case 1:
-				p2 = per3;
-				break;
-			case 2:
-				p2 = per4;
-				break;
-			case 3:
-				p2 = per5;
-				break;
-			case 4:
-				p2 = per6;
-				break;
-			case 5:
-				p2 = per7;
-				break;
-			case 6:
-				p2 = per8;
-				break;
-			case 7:
-				p2 = per9;
-				break;
-			case 8:
-				p2 = per10;
-				break;
-			}
-			break;
-		case 1:
-			p1 = per2;
-			switch (pe2) {
-			case 8:
-				p2 = per10;
-				break;
-			case 0:
-				p2 = per1;
-				break;
-			case 1:
-				p2 = per3;
-				break;
-			case 2:
-				p2 = per4;
-				break;
-			case 3:
-				p2 = per5;
-				break;
-			case 4:
-				p2 = per6;
-				break;
-			case 5:
-				p2 = per7;
-				break;
-			case 6:
-				p2 = per8;
-				break;
-			case 7:
-				p2 = per9;
-				break;
-			}
-			break;
-		case 2:
-			p1 = per3;
-			switch (pe2) {
-			case 8:
-				p2 = per10;
-				break;
-			case 0:
-				p2 = per1;
-				break;
-			case 1:
-				p2 = per2;
-				break;
-			case 2:
-				p2 = per4;
-				break;
-			case 3:
-				p2 = per5;
-				break;
-			case 4:
-				p2 = per6;
-				break;
-			case 5:
-				p2 = per7;
-				break;
-			case 6:
-				p2 = per8;
-				break;
-			case 7:
-				p2 = per9;
-				break;
-			}
-			break;
-		case 3:
-			p1 = per4;
-			switch (pe2) {
-			case 8:
-				p2 = per10;
-				break;
-			case 0:
-				p2 = per1;
-				break;
-			case 1:
-				p2 = per2;
-				break;
-			case 2:
-				p2 = per3;
-				break;
-			case 3:
-				p2 = per5;
-				break;
-			case 4:
-				p2 = per6;
-				break;
-			case 5:
-				p2 = per7;
-				break;
-			case 6:
-				p2 = per8;
-				break;
-			case 7:
-				p2 = per9;
-				break;
-			}
-			break;
-		case 4:
-			p1 = per5;
-			switch (pe2) {
-			case 8:
-				p2 = per10;
-				break;
-			case 0:
-				p2 = per1;
-				break;
-			case 1:
-				p2 = per2;
-				break;
-			case 2:
-				p2 = per3;
-				break;
-			case 3:
-				p2 = per4;
-				break;
-			case 4:
-				p2 = per6;
-				break;
-			case 5:
-				p2 = per7;
-				break;
-			case 6:
-				p2 = per8;
-				break;
-			case 7:
-				p2 = per9;
-				break;
-			}
-			break;
-		case 5:
-			p1 = per6;
-			switch (pe2) {
-			case 8:
-				p2 = per10;
-				break;
-			case 0:
-				p2 = per1;
-				break;
-			case 1:
-				p2 = per2;
-				break;
-			case 2:
-				p2 = per3;
-				break;
-			case 3:
-				p2 = per4;
-				break;
-			case 4:
-				p2 = per5;
-				break;
-			case 5:
-				p2 = per7;
-				break;
-			case 6:
-				p2 = per8;
-				break;
-			case 7:
-				p2 = per9;
-				break;
-			}
-			break;
-		case 6:
-			p1 = per7;
-			switch (pe2) {
-			case 0:
-				p2 = per1;
-				break;
-			case 8:
-				p2 = per10;
-				break;
-			case 1:
-				p2 = per2;
-				break;
-			case 2:
-				p2 = per3;
-				break;
-			case 3:
-				p2 = per4;
-				break;
-			case 4:
-				p2 = per5;
-				break;
-			case 5:
-				p2 = per6;
-				break;
-			case 6:
-				p2 = per8;
-				break;
-			case 7:
-				p2 = per9;
-				break;
-			}
-			break;
-		case 7:
-			p1 = per8;
-			switch (pe2) {
-			case 0:
-				p2 = per1;
-				break;
-			case 1:
-				p2 = per2;
-				break;
-			case 2:
-				p2 = per3;
-				break;
-			case 3:
-				p2 = per4;
-				break;
-			case 4:
-				p2 = per5;
-				break;
-			case 5:
-				p2 = per6;
-				break;
-			case 6:
-				p2 = per7;
-				break;
-			case 7:
-				p2 = per9;
-				break;
-			}
-			break;
-		case 8:
-			p1 = per9;
-			switch (pe2) {
-			case 8:
-				p2 = per10;
-				break;
-			case 0:
-				p2 = per1;
-				break;
-			case 1:
-				p2 = per2;
-				break;
-			case 2:
-				p2 = per3;
-				break;
-			case 3:
-				p2 = per4;
-				break;
-			case 4:
-				p2 = per5;
-				break;
-			case 5:
-				p2 = per6;
-				break;
-			case 6:
-				p2 = per7;
-				break;
-			case 7:
-				p2 = per8;
-				break;
-			}
-			break;
-		case 9:
-			p1 = per10;
-			switch (pe2) {
-			case 8:
-				p2 = per10;
-				break;
-			case 0:
-				p2 = per1;
-				break;
-			case 1:
-				p2 = per2;
-				break;
-			case 2:
-				p2 = per3;
-				break;
-			case 3:
-				p2 = per4;
-				break;
-			case 4:
-				p2 = per5;
-				break;
-			case 5:
-				p2 = per6;
-				break;
-			case 6:
-				p2 = per7;
-				break;
-			case 7:
-				p2 = per9;
-				break;
-			}
-			break;
+		ArrayList<String> people = new ArrayList<String>();
+		people.add("Obama");
+		people.add("Donald Trump");
+		people.add("Bernie Sanders");
+		people.add("Hillary Clintion");
+		people.add("Bill Clintion");
+		people.add("Modanna");
+		people.add("Tim Cook");
+		people.add("Sergey Brin");
+		people.add("Larry Page");
+		people.add("Edward Snowden");
+		people.add("Ozzy Osbourne");
+		people.add("Johnny Depp");
+		people.add("Arnold Schwarzenegger");
+		people.add("Morgan Freeman");
+		people.add("Will Smith");
+		people.add("George Clooney");
+		// people.add("");
+
+		String p1_random = people.get(new Random().nextInt(people.size()));
+		String p2_random = people.get(new Random().nextInt(people.size()));
+		if (p1_random == p2_random) {
+			p1_random = "Famous Person 1";
+			p2_random = "Famous Person 2";
+			System.out.println(p1_random + " " + p2_random);
+		} else {
+			System.out.println(p1_random + " " + p2_random);
 		}
 
-		System.out.println(p1);
+		System.out.println(p1_random);
+		System.out.println(p2_random);
 
-		System.out.println(p2);
+		ArrayList<String> actions = new ArrayList<String>();
+		actions.add(" wants to sue ");
+		actions.add(" is going to fight with ");
+		actions.add(" is mad at ");
+		actions.add(" is angry at ");
+		actions.add(" wants to hire ");
+		actions.add(" wants to kill ");
+		actions.add(" wants to marry ");
+		//actions.add("  ");
 
-		// actions
-		String act1 = " wants to sue ";
-		String act2 = " is going to talk to ";
-		String act3 = " is mad at ";
-		String act4 = " is angry at ";
-		String act5 = " wants to hire ";
-		String act6 = " wants to kill ";
-		String act7 = " wants to marry ";
 
-		// picks action
-		int act = rand.nextInt(7);
-		switch (act) {
-		case 0:
-			action = act1;
-			break;
-		case 1:
-			action = act2;
-			break;
-		case 2:
-			action = act3;
-			break;
-		case 3:
-			action = act4;
-			break;
-		case 4:
-			action = act5;
-			break;
-		case 5:
-			action = act6;
-			break;
-		case 6:
-			action = act7;
-			break;
-		}
-
+		String action = actions.get(new Random().nextInt(actions.size()));
 		System.out.println(action);
 
 		// picks type of news
-		int type = rand.nextInt(3);
-		switch (type) {
-		case 0:
-			bnews = "Breaking News: ";
-			break;
-		case 1:
-			bnews = "Breaking: ";
-			break;
-		case 2:
-			bnews = "Urgent Bulletin: ";
-			break;
-		}
-
+		ArrayList<String> newstype = new ArrayList<String>();
+		newstype.add("Breaking News: ");
+		newstype.add("Breaking: ");
+		newstype.add("Urgent Bulletin: ");
+		//newstype.add("SOMETHING: ");
+		
+		
+		String bnews = newstype.get(new Random().nextInt(newstype.size()));
 		System.out.println(bnews);
 
 		// prints final clickbait title
-		System.out.println(bnews + p1 + action + p2 + "!");
-		
+		System.out.println(bnews + p1_random + action + p2_random + "!");
+
 		try (PrintWriter out = new PrintWriter("Clickbait " + cal.getTimeInMillis() + ".txt")) {
 
 			out.println("Enjoy your clickbait!");
-			out.println(bnews + p1 + action + p2 + "!");
+			out.println(bnews + p1_random + action + p2_random + "!");
 
-		}
-		try {
-			Twitter twitter = new TwitterFactory().getInstance();
-
-			twitter.setOAuthConsumer(consumerKeyStr, consumerSecretStr);
-			AccessToken accessToken = new AccessToken(accessTokenStr,
-					accessTokenSecretStr);
-
-			twitter.setOAuthAccessToken(accessToken);
-
-			//Tweet Text here!
-			twitter.updateStatus(bnews + p1 + action + p2 + "! #TheBaitBot");
-
-			System.out.println("Successfully updated the status in Twitter.");
-		} catch (TwitterException te) {
-			te.printStackTrace();
 		}
 		
+		  try { Twitter twitter = new TwitterFactory().getInstance();
+		  
+		  twitter.setOAuthConsumer(consumerKeyStr, consumerSecretStr);
+		  AccessToken accessToken = new AccessToken(accessTokenStr,
+		  accessTokenSecretStr);
+		  
+		  twitter.setOAuthAccessToken(accessToken);
+		  
+		  // Tweet Text here! 
+		  twitter.updateStatus(bnews + p1_random + action + p2_random + "! #TheBaitBot");
+		  
+		  System.out.println("Successfully sent a tweet!"); } catch
+		  (TwitterException te) { te.printStackTrace(); }
+		 
+
 	}
 
 }
